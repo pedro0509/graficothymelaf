d1a769498f91ca16f7ed84e1a16d72371e7224cc
Pedro Manoel Rosa Domingues Santos <pedromanoel0509@hotmail.com>
2023-06-28 08:59:11 -0300

    Initial commit

---
+++ src/main/java/com/graficothymelaf/graficothymelaf/GraficothymelafApplication.java
@@ -0,0 +1,13 @@
+package com.graficothymelaf.graficothymelaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraficothymelafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraficothymelafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graficothymelaf/graficothymelaf/GraficothymelafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graficothymelaf.graficothymelaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraficothymelafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
