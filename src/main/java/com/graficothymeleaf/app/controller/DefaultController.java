8aecf1977f13f05bdc28f8b23419486399e2f97a
Pedro Manoel Rosa Domingues Santos <pedromanoel0509@hotmail.com>
2023-07-02 23:44:48 -0300

    Imprimindo gráfico na tela.

--- src/main/java/com/graficothymeleaf/app/controller/DefaultController.java
+++ src/main/java/com/graficothymeleaf/app/controller/DefaultController.java
@@ -1,17 +1,17 @@
 package com.graficothymeleaf.app.controller;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -23,35 +23,43 @@ public class DefaultController {
 	Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
 	@GetMapping("/")
-	public ModelAndView inicio(Model model) throws IOException {
+	public String index(Model model) throws IOException {
+		model.addAttribute("grafico", getChartData());
+		return "inicio";
+	}
+
+	private List<Object> getChartData() throws IOException {
 
 		WebClientSelic selic = new WebClientSelic();
 
 		JSONArray ret = selic.buscaSerieTxSelicUltimosValores(500);
 
-		Map<String, Double> graphDataSelic = new TreeMap<>();
-		
-		try {
+		List<Object> regis = new ArrayList<>();
 
-			List<Registro> users = new Gson().fromJson(ret.toString(), new TypeToken<List<Registro>>() {
-			}.getType());
+		regis.add(new Object[] { "Data", "Valor" });
 
-			users.stream().forEach(limp -> graphDataSelic.put(limp.getMesAno(), limp.getValor()));
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		for (int i = 0; i < ret.length(); i++) {
+			JSONObject o = ret.getJSONObject(i);
+			regis.add(new Object[] { o.getString("data"), o.getBigDecimal("valor") });
 		}
 
-		ModelAndView modelAndView = new ModelAndView("inicio");
-		modelAndView.addObject("grafico", graphDataSelic);
+//		try {
+//
+//			List<Registro> users = new Gson().fromJson(ret.toString(), new TypeToken<List<Registro>>() {
+//			}.getType());
+//
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//		}
 
-		return modelAndView;
+		return regis;
+
 	}
 
 	public class Registro {
 
 		private String data;
-		private Double valor;
+		private BigDecimal valor;
 
 		public String getMesAno() {
 			return data;
@@ -61,15 +69,15 @@ public class DefaultController {
 			this.data = data;
 		}
 
-		public Double getValor() {
+		public BigDecimal getValor() {
 			return valor;
 		}
 
-		public void setValor(Double valor) {
+		public void setValor(BigDecimal valor) {
 			this.valor = valor;
 		}
 
-		public Registro(String data, Double valor) {
+		public Registro(String data, BigDecimal valor) {
 			this.data = data;
 			this.valor = valor;
 		}
